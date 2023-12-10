cb708c93d672b7b827d5fd6261142d7a76ca552d
agn22x <agn22x@proton.me>
2023-12-10 18:33:18 +0530

    Called printf from asm

---
+++ x86-64/printf/printf.s
@@ -0,0 +1,19 @@
+.intel_syntax noprefix
+
+.text
+.globl main
+main:
+sub rsp, 8
+lea rdi, fmt[rip]
+mov rsi, 2
+mov rdx, 3
+mov rcx, 5
+xor rax, rax
+call printf
+add rsp, 8
+xor rax, rax
+ret
+
+.section .rodata
+fmt:
+.string "%d + %d is %d\n"
